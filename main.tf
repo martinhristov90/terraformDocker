b41a84f87721d313aff79ffb5e7b47d7150a22bc
martinhristov90 <49191915+martinhristov90@users.noreply.github.com>
2019-06-03 15:02:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
                 
+# Getting Nginx image.
 resource "docker_image" "nginx" {
   name = "nginx:1.11-alpine"
 }
+# Running two Nginx containers.
 resource "docker_container" "nginx-server" {
   count = 2
+  # Naming containers nginx-server-1 and nginx-server-2
   name = "nginx-server-${count.index+1}"
   image = "${docker_image.nginx.latest}"
   ports {
