bae5cd5b3afbc86535ba8ffaf583bc896617c6fc
Martin Hristov <martinhristov@Martins-MBP.lan>
2019-06-03 16:25:42 +0300

    using terraform fmt and using own boxes

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
-                
 resource "docker_image" "nginx" {
   name = "nginx:1.11-alpine"
 }
+
 resource "docker_container" "nginx-server" {
   count = 2
-  name = "nginx-server-${count.index+1}"
+  name  = "nginx-server-${count.index+1}"
   image = "${docker_image.nginx.latest}"
+
   ports {
     internal = 80
   }
+
   volumes {
-    container_path  = "/usr/share/nginx/html"
-    host_path = "/home/vargrant/html_docs"
-    read_only = true
+    container_path = "/usr/share/nginx/html"
+    host_path      = "/home/vargrant/html_docs"
+    read_only      = true
   }
 }
